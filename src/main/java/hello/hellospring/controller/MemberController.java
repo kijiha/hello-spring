97cbf5512facff0efea613e5b9e339915a2ce448
myLaptop1 <r48713@gmail.com>
2022-10-30 21:31:10 +0900

    just before D/B install

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,53 @@
+package hello.hellospring.controller;
+
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Vector;
+
+// 스프링 컨터이너에 등록하는 어노테이션
+//
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired // 연관된 객체를 컨터이너에서 찾아서 연결시키는 어노테이션
+    // 의존성 주입 (Dependency Injection
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMembersForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMember();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+
+}
--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -25,6 +25,7 @@ class MemberServiceTest {
 
     @AfterEach
     public void afterEach() {
+
         memberRepository.clearStore();
     }
 
@@ -42,8 +43,29 @@ class MemberServiceTest {
         assertEquals(member.getName(),findMember.getName());
     }
 
+
+
+    @Test
+    public void 중복회원_예외() throws Exception {
+        Member member1 = new Member();
+        member1.setName("m1");
+
+        Member member2 = new Member();
+        member2.setName("m1");
+
+
+        //When
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+    }
+
+    // 전체 회원 조회
     @Test
     void findMember() {
+
     }
 
     @Test
